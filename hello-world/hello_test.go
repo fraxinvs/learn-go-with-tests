97fb429d0db32fd17b447ddd9a4eb035a9959d31
fraxinvs <fraxinvs>
2024-12-14 08:03:41 +0000

    (hello-world): add hello world v4

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func Test_Hello(t *testing.T) {
-	got := Hello("John")
-	want := "Hello, John"
+	t.Run("should reply hello to given person", func(t *testing.T) {
+		got := Hello("John")
+		want := "Hello, John"
 
-	if got != want {
-		t.Errorf("got %q want %q ", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q ", got, want)
+		}
+	})
+
+	t.Run("should reply 'Hello, World' when given an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q ", got, want)
+		}
+	})
 }
