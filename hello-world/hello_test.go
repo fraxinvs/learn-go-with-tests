9d05c8ca5c6e7649c79158bc8da2e2280e9fef68
fraxinvs <fraxinvs>
2024-12-14 08:27:02 +0000

    (hello-world): add hello world v6

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func Test_Hello(t *testing.T) {
 	t.Run("should reply hello to given person", func(t *testing.T) {
-		got := Hello("John")
+		got := Hello("John", "English")
 		want := "Hello, John"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("should reply 'Hello, World' when given an empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("should reply with Spanish greeting", func(t *testing.T) {
+		got := Hello("Marc", "Spanish")
+		want := "Hola, Marc"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("should reply with French greeting", func(t *testing.T) {
+		got := Hello("Jules", "French")
+		want := "Bonjour, Jules"
 
 		assertCorrectMessage(t, got, want)
 	})
