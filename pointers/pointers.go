3ea714f8954dce6ec20ccab5b255de9161668607
fraxinvs <fraxinvs>
2024-12-17 20:38:56 +0000

    (pointers): add pointers v2

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,6 +1,15 @@
 package pointers
 
+import "fmt"
+
+type Stringer interface {
+	string() string
+}
 type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
@@ -12,4 +21,8 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -3,13 +3,25 @@ package pointers
 import "testing"
 
 func Test_Wallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+		if got != want {
+			t.Errorf("got: %d, want: %d", got, want)
+		}
 	}
+	t.Run("should deposit 10 bitcoin in wallet", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("should withdraw 10 bitcoin in wallet", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
