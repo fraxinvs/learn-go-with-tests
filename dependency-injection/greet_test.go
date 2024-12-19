3937600bd03d6c4ff1e9981f4018c6a225e54580
fraxinvs <fraxinvs>
2024-12-19 12:06:17 +0000

    (dependency-injection): add dependency injection v1

--- dependency-injection/greet_test.go
+++ dependency-injection/greet_test.go
@@ -1,1 +1,18 @@
 package dependencyinjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_Greet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "John")
+
+	got := buffer.String()
+	want := "Hello, John"
+
+	if got != want {
+		t.Errorf("got: %q, want: %q", got, want)
+	}
+}
