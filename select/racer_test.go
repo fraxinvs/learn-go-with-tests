7de8879315d378abb13f813bb1289bb4d539b948
fraxinvs <fraxinvs>
2024-12-21 10:59:44 +0000

    (select): add select v8

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,24 +8,42 @@ import (
 )
 
 func Test_Racer(t *testing.T) {
-	slowServer := makeDeplayedServer(20 * time.Millisecond)
-	fastServer := makeDeplayedServer(0 * time.Millisecond)
+	t.Run("should compare speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got: %q, want: %q", got, want)
-	}
+		if err != nil {
+			t.Fatalf("did not expect an error but got one: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
+	})
+
+	t.Run("should return an error if a server does not respond within timeout", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but did not get one")
+		}
+	})
 }
 
-func makeDeplayedServer(delay time.Duration) *httptest.Server {
+func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
