25caf6bc0ed80e8c461ad6ddde9207c01bc42dc1
fraxinvs <fraxinvs>
2024-12-21 10:14:11 +0000

    (select): add select v2

--- select/racer.go
+++ select/racer.go
@@ -1,1 +1,22 @@
 package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	_, _ = http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	_, _ = http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
--- select/racer_test.go
+++ select/racer_test.go
@@ -1,1 +1,28 @@
 package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func Test_Racer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got: %q, want: %q", got, want)
+	}
+}
