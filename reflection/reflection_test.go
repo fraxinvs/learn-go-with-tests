73e007ca109ef61ff9f81b85e4aa2ad02dc30dd4
fraxinvs <fraxinvs>
2024-12-23 10:37:02 +0000

    (reflection): add reflection v1

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_Walk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct{ Name string }{"John"},
+			[]string{"John"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"John", "London"},
+			[]string{"John", "London"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got: %v, want: %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
