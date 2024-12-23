4b22b488cdb1b55b1f57ca333600e00013406491
fraxinvs <fraxinvs>
2024-12-23 11:12:15 +0000

    (reflection): add reflection v9

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -93,6 +93,27 @@ func Test_Walk(t *testing.T) {
 		assertContains(t, got, "Moo")
 		assertContains(t, got, "Baa")
 	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Tokyo"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Tokyo"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
 }
 
 type Person struct {
