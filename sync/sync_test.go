aa61c2797977cb246299183e92acdca4cac7b937
fraxinvs <fraxinvs>
2024-12-24 16:08:10 +0000

    (sync): add sync v1

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,1 +1,20 @@
-package sync_test
+package sync
+
+import "testing"
+
+func Test_Counter(t *testing.T) {
+	t.Run("should increment counter by 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	if got.Value() != want {
+		t.Errorf("got: %d, want: %d", got.Value(), 3)
+	}
+}
