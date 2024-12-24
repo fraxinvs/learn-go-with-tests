965786703f922ee2525cfe88b9ac345b5bf3bdfd
fraxinvs <fraxinvs>
2024-12-24 16:28:19 +0000

    (sync): add sync v2

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,19 +1,40 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func Test_Counter(t *testing.T) {
 	t.Run("should increment counter by 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("should run safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	if got.Value() != want {
 		t.Errorf("got: %d, want: %d", got.Value(), 3)
 	}
